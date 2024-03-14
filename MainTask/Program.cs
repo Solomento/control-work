2913ef4377670056942cb88f85dd65a0f021485d
Alex <alex.dvoynev2014@yandex.ru>
2024-03-14 11:46:22 +0300

    Написана основная программа

---
+++ MainTask/Program.cs
@@ -0,0 +1,31 @@
+﻿void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите размер массива: ");
+int elementsCount = int.Parse(Console.ReadLine());
+string[] array1 = new string[elementsCount];
+for (int i = 0; i < array1.Length; i++)
+{
+    Console.Write($"Ввод данных {i}: ");
+    array1[i] = Console.ReadLine();
+}
+string[] array2 = new string[array1.Length];
+SecondArray(array1, array2);
+PrintArray(array2);
